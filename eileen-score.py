549c79aea27cae2cfe810b71e98ffdcb68627632
Eric Whyne <ericwhyne@gmail.com>
2017-02-22 19:36:52 -0500

    score reports only for last month

--- eileen-score.py
+++ eileen-score.py
@@ -6,6 +6,7 @@ import json
 import sys
 import re
 from datetime import datetime, timedelta
+from monthdelta import monthdelta
 import glob, os
 from collections import Counter
 
@@ -16,48 +17,54 @@ config = yaml.safe_load(open(config_file))
 
 logdir = expanduser("~") + '/.bigtime/log/'
 
+start_date_obj = datetime.now() - monthdelta(1)
+
 days_of_week = ['Monday','Tuesday','Wednesday',
     'Thursday','Friday','Saturday','Sunday']
 
 employee_score = Counter()
 employee_days = Counter()
 for logfilename in glob.glob(logdir + "timecard-data-*.json"):
-    print(logfilename)
+    #print(logfilename)
     with open(logfilename, 'r') as logfile:
         timecard_data = json.load(logfile)
     logdate = re.sub(logdir + "timecard-data-", '', logfilename)
     logdate = re.sub("\.json",'', logdate)
-    print logdate
-    #print timecard_data
-    employees = {}
-    try:
-        tc_data = timecard_data['Data']
-        for record in tc_data:
-            name = record[4]
-            date = record[9]
-            if name in employees.keys():
-                if date not in employees[name]:
-                    employees[name].append(date)
-            else:
-                employees[name] = [date]
-        dayobj = datetime.strptime(logdate, "%Y-%m-%d") - timedelta(days=1)
-        day = dayobj.strftime("%Y-%m-%d")
-        dow = datetime.strptime(day, "%Y-%m-%d").weekday()
-        if dow > 4: # don't show results for weekend days
-            continue
-        for name in employees.keys():
-            if name not in config['ignore']:
-                employee_days[name] += 1
-                if day in employees[name]:
-                    employee_score[name] += 1
-    except KeyError:
-        print "Bad data in ", logfilename
-        pass
+    logdate_obj = datetime.strptime(logdate, "%Y-%m-%d")
+
+    if logdate_obj > start_date_obj:
+        print logdate, ">", start_date_obj.strftime("%Y-%m-%d")
+        #print timecard_data
+        employees = {}
+        try:
+            tc_data = timecard_data['Data']
+            for record in tc_data:
+                name = record[4]
+                date = record[9]
+                if name in employees.keys():
+                    if date not in employees[name]:
+                        employees[name].append(date)
+                else:
+                    employees[name] = [date]
+            dayobj = datetime.strptime(logdate, "%Y-%m-%d") - timedelta(days=1)
+            day = dayobj.strftime("%Y-%m-%d")
+            dow = datetime.strptime(day, "%Y-%m-%d").weekday()
+            if dow > 4: # don't show results for weekend days
+                continue
+            for name in employees.keys():
+                if name not in config['ignore']:
+                    employee_days[name] += 1
+                    if day in employees[name]:
+                        employee_score[name] += 1
+        except KeyError:
+            print "Bad data in ", logfilename
+            pass
 
 report = ""
 report_dow = datetime.strptime(today_date, "%Y-%m-%d").weekday()
-report += "Timecard Compliance Scores as of "
-report += today_date + " " + days_of_week[report_dow] + "\n"
+report += "Timecard Compliance Scores since "
+report += start_date_obj.strftime("%Y-%m-%d") + "\n"
+
 employee_names_to_report = employee_days.keys()
 employee_names_to_report.sort()
 for employee in employee_names_to_report:
